8c377107caedec7306de230747dbbc500b44128a
Jostein04 <94624601+Jostein04@users.noreply.github.com>
2022-12-12 13:52:34 +0100

    Update tekstbasertspill.py

--- tekstbasertspill.py
+++ tekstbasertspill.py
@@ -8,13 +8,27 @@ def jægerHistorie():
     userInput = input()
     if userInput == "Dreper han":
       print("Du velger å drepe han og deretter fullføre oppdraget, dette var en sentral person for den russiske siden og du får æresmedalje for din tjeneste.")
-      quit()
+      jægerhistorieEtterliv()
     elif userInput == "Dreper han ikke":
       print("Du velger å la han gå, han klarer å skaffe viktig informasjon om Norge sin plan fremover i krigen, din øverste kommandant finner ut av at du lot han gå og du får dødsstraff. ")
       quit()
     else:
       print("Venligst skriv inn et svar, Dreper han/Dreper han ikke.")
-
+def jægerhistorieEtterliv():
+    directions = ["tar kontakt med folk","holder deg for deg selv"]
+    print("Det er 5 år siden den kalde dagen. Du har vunnet krigen og fått æresmedalje og er en nasjonal helt. Men du er alene og uten familie det føles ikke ut som du har vunnet noe av krigen. Du har mange dårlige tanker om framtiden av livet ditt, du går på piller for posttraumatisk stresslidelse. Hva er det neste du gjør?")
+    userInput = ""
+    while userInput not in directions:
+      print("Valg: tar kontakt med folk/holder deg for deg selv")
+      userInput = input()
+      if userInput == "tar kontakt med folk":
+        print("Du finner noen venner som er veldig interest i frisbeegolf, du blir veldig god etter noen år og deltar i norgesmesterskapet og vinner gull. Du går proffesjonel og lever resten av livet som en krigshelt og frisbeegolf proff.")
+        quit()
+      elif userInput == "holder deg for deg selv":
+        print("Du holder deg hjemme og har vært deprimert i flere måneder og har kjent på at det ikke fantes noen vei ut av den uendelige mørket. Du ombestemmer deg å prøver å søke hjelp, men det har ikke hjulpet. Etter mye grubling kommer du frem til at det eneste som kan løse problemet ditt, var å ta sitt eget liv. En dag bestemte du deg for å gjøre det. Du forberedte deg, og tar avskjed med familien din uten å si hva du skal. Du henger deg selv i kjelleren, og i det du dør, begynte din lange reise til den andre siden.")
+        quit()
+      else:
+        print("Vennligst skriv inn et gyldig svar, La de gå/Ikke la de gå")
 def hærensValg():
   actions = ["Jægersoldat","Artillerist"]
   print("De er altid fint å ha flere i hæren hvilken stilling ønsker du å ha?")
@@ -23,8 +37,7 @@ def hærensValg():
     print("Options: Jægersoldat/Artillerist")
     userInput = input()
     if userInput == "Jægersoldat":
-      if weapon:
-        jægerHistorie()
+      jægerHistorie()
     elif userInput == "Artillerist":
       artilleristHistorie()
     else:
@@ -85,7 +98,7 @@ def PilotSkyter():
       print("Du rapporterer drapene du har gjort på de sivile og blir fratatt rollen som pilot.")
       quit()
     elif userInput == "Nei":
-      Print("Du rapporterer alt utenom de sivile og svekker den russiske frontlinjen ")
+      print("Du rapporterer alt utenom de sivile og svekker den russiske frontlinjen ")
       quit()
     else:
       print("Vennligst skriv inn et gyldig svar, Ja/Nei")
@@ -202,8 +215,7 @@ def artilleristHistorie():
       print("Du planter bomben og det norske forsvaret finner ut at det var deg og dreper deg på stedet")
       quit()
     else:
-      print("Vennligst skriv inn et gyldig svar, planter ikke bomben/planter bombenj")
-
+      print("Vennligst skriv inn et gyldig svar, planter ikke bomben/planter bomben")
 def introScene():
   directions = ["Ja","nei"]
   print("Du har blitt spurt om å dra til militæret, har du lyst til dette?")
