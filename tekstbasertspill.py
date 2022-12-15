4e6d65e6a712f97fa0e584aef43df01587fbeaa7
Oevenrud <90458363+Oevenrud@users.noreply.github.com>
2022-12-15 08:50:51 +0100

    Update tekstbasertspill.py

--- tekstbasertspill.py
+++ tekstbasertspill.py
@@ -4,7 +4,7 @@ def jægerHistorie()-> None:
   print()
   print("Du får et oppdrag om å følge en spesialtrent russisk soldat som befinner seg på norsk territorium, når du ser soldaten finner du ut at det er en du har hatt førstegangstjeneste med. Hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Dreper han 2 - Dreper han ikke /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Dreper han".lower() or userInput.lower()=="1":
@@ -19,11 +19,11 @@ def jægerHistorie()-> None:
       print("Venligst skriv inn et svar, 1 - Dreper han 1 - Dreper han ikke.")
 
 def jægerhistorieEtterliv()->None:
-    """Gir deg mulighet til å velge mellom alternativene i directions."""
+    """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
     directions = ["tar kontakt med folk","holder deg for deg selv","hjelp"]
     print("Det er 5 år siden den kalde dagen. Du har vunnet krigen og fått æresmedalje og er en nasjonal helt. Men du er alene og uten familie det føles ikke ut som du har vunnet noe av krigen. Du har mange dårlige tanker om framtiden av livet ditt, du går på piller for posttraumatisk stresslidelse. Hva er det neste du gjør?")
     userInput = ""
-    while userInput not in directions:
+    while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
       print("Valgmuligheter: 1 - tar kontakt med folk 2 - holder deg for deg selv /skriv hjelp eller 3 hvis du ikke forstår")
       userInput = input()
       if userInput.lower() == "tar kontakt med folk".lower() or userInput.lower()=="1":
@@ -37,11 +37,12 @@ def jægerhistorieEtterliv()->None:
       else:
         print("Vennligst skriv inn et gyldig svar, 1 - La de gå 2 - Ikke la de gå")
 
-def hærensValg():
+def hærensValg()->None:
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   actions = ["Jægersoldat","Artillerist","hjelp"]
   print("De er altid fint å ha flere i hæren hvilken stilling ønsker du å ha?")
   userInput = ""
-  while userInput not in actions:
+  while userInput not in actions:#Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Jægersoldat 2 - Artillerist /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Jægersoldat".lower() or userInput.lower()=="1":
@@ -54,10 +55,11 @@ def hærensValg():
       print("Vennligst skriv inn et gyldig svar, 1 - Jægersoldat 2 - Artillerist.")
 
 def luftforsvaretValg():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Fallskjermjeger","Pilot","hjelp"]
   print("Hva ønsker du å bli?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Fallskjermjeger 2 - Pilot /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Fallskjermjeger".lower() or userInput.lower()=="1":
@@ -70,10 +72,11 @@ def luftforsvaretValg():
       print("Vennligst skriv inn et gyldig svar, 1 - Fallskjermjeger 2 - Pilot")
 
 def Pilot():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Skyter","Skyter ikke","hjelp"]
   print("Du får beskjed om å skyte opp en russisk leir, men du vet at det er sivile der inne, hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Skyter 2 - Skyter ikke /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Skyter".lower() or userInput.lower()=="1":
@@ -86,10 +89,11 @@ def Pilot():
       print("Vennligst skriv inn et gyldig svar, 1 - Skyter 2 - Skyter ikke")
 
 def PilotSkyterIkke():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Aksepter straff","Nekt straff","hjelp"]
   print("Du skyter leiren og treffer de sivile også.Du blir bedt om å rapportere din side om oppdraget, forteller du dem om de sivile i sonen?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Aksepter straff 2 - Nekt straff /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Aksepter straff".lower() or userInput.lower()=="1":
@@ -104,10 +108,11 @@ def PilotSkyterIkke():
       print("Vennligst skriv inn et gyldig svar, 1 - Aksepter straff 2 - Nekt straff")
 
 def PilotSkyter():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Ja","Nei","hjelp"]
   print("Du skyter leiren og treffer de sivile også.Du blir bedt om å rapportere din side om oppdraget, forteller du dem om de sivile i sonen?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Ja 2 - Nei /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Ja".lower() or userInput.lower()=="1":
@@ -122,10 +127,11 @@ def PilotSkyter():
       print("Vennligst skriv inn et gyldig svar,  1 - Ja 2 - Nei")
 
 def Fallskjermjeger():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Gi til piloten","Ta den selv","hjelp"]
   print("Flyet du sitter på i blir skutt av russiske krigsfly og 1 fallskjerm detter ut. Resten av bataljonen din hopper ut og det er kun deg og piloten igjen. Det er bare en fallskjerm igjen, hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - Gi til piloten 2 - Ta den selv /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Gi til piloten".lower() or userInput.lower()=="1":
@@ -139,10 +145,11 @@ def Fallskjermjeger():
       print("Vennligst skriv inn et gyldig svar, 1 - Gi til piloten 2 - Ta den selv")
       
 def TaDenSelv():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Dette er en av de mulige endingene. Valgmulighetene er de som er i listen directions."""
   directions = ["Skyt russisk soldat","Snik unna","hjelp"]
   print("Du tar fallskjermen selv og klarer å hoppe ut av flyet. Du lander i en russisk sone. Du ser en russisk soldat som har fanget en sivil. Hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter: 1 - skyt russisk soldat 2 - snik unna /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Skyt russisk soldat".lower() or userInput.lower()=="1":
@@ -156,18 +163,19 @@ def TaDenSelv():
     else:
       print("Vennligst skriv inn et gyldig svar, Skyt russisk soldat/Snik unna")
 
-Militærvalg={
+Militærvalg={ #Ordliste som er de forskjellige valgmulighetene i starten, påvirker mye av programmet videre.
   "Luftforsvaret":"luftforsvaret",
   "Hæren":"hæren",
   "Sjøforsvaret":"sjøforsvaret",
   "Hjelp":"hjelp"
 }
 
-def militærvalg():
+def militærvalg()->None:
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken del av spillet du blir sendt videre til. Valgmulighetene er de i ordboken 'Militærvalg'."""
   directions = ["Luftforsvaret","Hæren","Sjøforsvaret","Hjelp"]
   print("Så bra du vil ble med i militæret, her finns det masse muligheter hva ønsker du å gjøre?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print(f"Du kan velge mellom 1 - {Militærvalg['Luftforsvaret']}, 2 - {Militærvalg['Hæren']}, 3 - {Militærvalg['Sjøforsvaret']}, 4 - {Militærvalg['Hjelp']}.")
     userInput = input()
     userInput=userInput.lower()
@@ -183,11 +191,12 @@ def militærvalg():
       print("Vennligst skriv inn et gyldig svar, 1 - Luftforsvaret 2 - Hæren 3 - sjøforsvaret")
       
 
-def sjøforsvaretValg():
+def sjøforsvaretValg()->None:
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["Kystvakt","Båtoperatør","hjelp"]
   print("Hva ønsker du å bli?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valgmuligheter:  1 - Kystvakt 2 - Båtoperatør /skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Kystvakt".lower() or userInput.lower()=="1":
@@ -200,10 +209,11 @@ def sjøforsvaretValg():
       print("Vennligst skriv inn et gyldig svar, 1 - Kystvakt 2 - Båtoperatør")
 
 def Kystvakt():
+    """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til, er også en mulig ending. Valgmulighetene er de som er i listen directions."""
     directions = ["La de gå","Undersøker nærmere","hjelp"]
     print("Krigen har vart i 3 år nå, det er ekstrem sult og fattigdom i nord russiske hjem. Du er på vakt å tar noen i å snikfiske i norske vann. Hva gjør du?")
     userInput = ""
-    while userInput not in directions:
+    while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
       print("Valgmuligheter: 1 - La de gå 2 - Undersøker nærmere skriv hjelp eller 3 hvis du ikke forstår")
       userInput = input()
       if userInput.lower() == "La de gå".lower() or userInput.lower()=="1":
@@ -216,11 +226,12 @@ def Kystvakt():
       else:
         print("Vennligst skriv inn et gyldig svar, 1 - La de gå 2 - Undersøker nærmere")
 
-def undersøke():
+def undersøke()->None:
+    """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til, er også en mulig ending. Valgmulighetene er de som er i listen directions."""
     directions = ["Sniker seg innpå","Gjør deg synlig","hjelp"]
     print("Ved en nærmere kikk ser du at det viser seg at de ikke fisker men gjør noe annet, De har ikke sett deg enda. Du har valget mellom å gjøre deg synlig eller snike deg innpå Hva gjør du?")
     userInput = ""
-    while userInput not in directions:
+    while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
       print("Valg: 1 - Sniker seg innpå 2 - Gjør deg synlig /skriv hjelp eller 3 hvis du ikke forstår")
       userInput = input()
       if userInput.lower() == "Gjør deg synlig".lower() or userInput.lower()=="1":
@@ -235,10 +246,11 @@ def undersøke():
 
 
 def Snike():
+    """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Dette er en av avslutningene i programmet/spillet. Valgmulighetene er de som er i listen directions."""
     directions = ["Rapporter det","Ikke rapporter","hjelp"]
     print("Du og båten din klarer å snike deg innpå den russiske båten, dere klarer og komme dere på båten deres og ser at det er en russisk soldat med sin familie som henter opp krabbeteiner. Ønsker du å rapportere det til din øverste kommandant? ")
     userInput = ""
-    while userInput not in directions:
+    while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
       print("Valg: 1 - Rapporter det 2 - Ikke rapporter /skriv hjelp eller 3 hvis du ikke forstår")
       userInput = input()
       if userInput.lower() == "Rapporter det".lower() or userInput.lower()=="1":
@@ -253,11 +265,12 @@ def Snike():
         print("Vennligst skriv inn et gyldig svar, 1 - Rapporter det 2 - Ikke rapporter")
 
 
-def Båtoperatør():
+def Båtoperatør()->None:
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Dette er en av avlutningene spilleren kan ha. Valgmulighetene er de som er i listen directions."""
   directions = ["Varsle","Ikke varsle","hjelp"]
   print("Du får vite at båten du styrer er et mål for russerne, du får også beskjed om å ikke varsle de andre om bord siden dette kan skape panikk. Hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:#Sammenligner brukeren sin input med valgmulighetene
     print("Valg: 1 - Varsle 2 - Ikke varsle Skriv hjelp eller 3 hvis du ikke forstår")
     userInput = input()
     if userInput.lower() == "Varsle".lower() or userInput.lower()=="1":
@@ -271,15 +284,17 @@ def Båtoperatør():
     else:
       print("Vennligst skriv inn et gyldig svar, 1 - Varsle 2 - Ikke varsle")
       
-def ikkemilitær():
+def ikkemilitær()->None:
+  """En mulig avlsutning på spillet, korteste mulige gjennomføring av spillet"""
   print("Norge hadde for lite soldater til krigen, Russland har tatt over landet og du har blitt sendt til en konsentrasjonsleir i Sibir")
   quit()
 
 def artilleristHistorie():
+  """gir deg valgmuligheten mellom ulike utfall av spillet, skriv inn enten en av alternativene, eller tallene. Basert på hva du velger vil dette påvirke hvilken funksjon du blir sendt videre til. Valgmulighetene er de som er i listen directions."""
   directions = ["planter ikke bomben","planter bomben","hjelp"]
   print("Du fikk høre om denne krigen når du hadde en tur til Moskva for 5 år siden, da fikk du et eget oppdrag om å bli med i det norske forsvaret, du får oppdrag om å plante en bombe i en norsk leir. Hva gjør du?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valg: 1 - planter ikke bomben 2 - planter bomben /Hvis du ikke skjønner skriv hjelp eller 3/")
     userInput = input()
     if userInput.lower() == "planter ikke bomben".lower() or userInput.lower()=="1":
@@ -293,11 +308,12 @@ def artilleristHistorie():
     else:
       print("Vennligst skriv inn et gyldig svar, 1 - planter ikke bomben 2 - planter bomben")
 
-def introScene()-> None:
+def introScene():
+  """Introscenen som starter programmet, skriv inn et av alternativene i listen directions, Påvirker historien/programmet mye. alle som kjører koden kommer innom denne funksjonen."""
   directions = ["Ja","nei","Hjelp"]
   print("Du har blitt spurt om å dra til militæret, har du lyst til dette?")
   userInput = ""
-  while userInput not in directions:
+  while userInput not in directions:  #Sammenligner brukeren sin input med valgmulighetene
     print("Valg: 1 - Ja 2 - Nei /Hvis du ikke skjønner skriv hjelp eller 3/")
     userInput = input()
     if userInput.lower() == "Nei".lower() or userInput.lower()=="2":
@@ -309,8 +325,7 @@ def introScene()-> None:
     else: 
       print("Vennligst skriv inn et gyldig svar, 1 - Ja 2 - Nei")
     
-
-if __name__ == "__main__":
+if __name__ == "__main__": #starten av koden, tar inn brukeren sitt navn
   while True:
     print("Velkommen til dette tekstbaserte spillet av Oda og Jostein!")
     print("La oss starte med navnet ditt: ")
